94ca93d46caa514f03934a0bb50ef5cac5f062ec
Ozsumit <129023326+Ozsumit@users.noreply.github.com>
2024-10-03 14:29:55 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 
 // Automatically redirect after 3 seconds (3000 milliseconds)
 setTimeout(function() {
-  window.location.href = 'https://example.com'; // Replace with your target URL
+  window.location.href = 'https://donation-game.vercel.app/'; // Replace with your target URL
 }, 1000);
