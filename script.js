114472c7928ccf6508015b057ba8a4b4ed5eaadf
Ozsumit <129023326+Ozsumit@users.noreply.github.com>
2024-10-03 14:25:58 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Automatically redirect after 3 seconds (3000 milliseconds)
+setTimeout(function() {
+  window.location.href = 'https://example.com'; // Replace with your target URL
+}, 1000);
